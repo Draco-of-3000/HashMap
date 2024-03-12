1ef3b06a940369a54137b634d3b4a4af3e25960d
Draco <kevin.tenisson@gmail.com>
2024-03-12 08:03:05 -0700

    Initalize method

--- hashmap.rb
+++ hashmap.rb
@@ -1,4 +1,10 @@
 class HashMap
+    def initialize
+        @buckets = Array.new(16) { [] }
+        @load_factor = 0.75
+        @size = 0
+    end
+
     def hash(key)
         hash_code = 0
         prime_number = 31
