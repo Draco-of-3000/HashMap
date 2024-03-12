2eef20f53f35c528a0062805026b554a867e8d72
Draco <kevin.tenisson@gmail.com>
2024-03-12 08:24:32 -0700

    Added set  method

--- hashmap.rb
+++ hashmap.rb
@@ -1,6 +1,6 @@
 class HashMap
     def initialize
-        @buckets = Array.new(16) { [] }
+        @buckets = Array.new(16) { [] } #Initialize with separate chaining to handle potential collissions
         @load_factor = 0.75
         @size = 0
     end
@@ -13,4 +13,27 @@ class HashMap
            
         hash_code
     end
+
+    def set(key, value)
+        index = hash(key) % @buckets.length
+        bucket = @buckets[index]
+
+        #Check for existing key in the bucket
+        pair_exists? = bucket.find { |pair| pair[0] == key}
+
+        if pair_exists? 
+            #Update the value if the key already exists
+            pair_exists?[1] = value 
+        else 
+            # Add a new key-value pair to the bucket
+            bucket << [key, value]
+            @size += 1
+
+            #Check if the load factor is exceeded, and grow the buckets if needed
+            grow_buckets if @size.to_f / @buckets.length > @load_factor
+
+        end
+    end
+
+    
 end     
